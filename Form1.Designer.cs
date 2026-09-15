96bec651243e58bae6294c07b0a225d22473f63e
Djcarrier <rageofcarrier@gmail.com>
2026-09-14 21:53:12 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Cadastro_2
+{
+    partial class Frm_cadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_Nome = new System.Windows.Forms.Label();
+            this.Lbl_Sobrenome = new System.Windows.Forms.Label();
+            this.Lbl_Idade = new System.Windows.Forms.Label();
+            this.Lbl_Bairro = new System.Windows.Forms.Label();
+            this.Lbl_Celular = new System.Windows.Forms.Label();
+            this.Lbl_Conf_dados = new System.Windows.Forms.Label();
+            this.Txt_Nome = new System.Windows.Forms.TextBox();
+            this.Txt_Sobrenome = new System.Windows.Forms.TextBox();
+            this.Txt_Idade = new System.Windows.Forms.TextBox();
+            this.Txt_Bairro = new System.Windows.Forms.TextBox();
+            this.Txt_Cellular = new System.Windows.Forms.TextBox();
+            this.Txt_Conf_Dados = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lbl_Nome
+            // 
+            this.Lbl_Nome.AutoSize = true;
+            this.Lbl_Nome.Location = new System.Drawing.Point(111, 134);
+            this.Lbl_Nome.Name = "Lbl_Nome";
+            this.Lbl_Nome.Size = new System.Drawing.Size(35, 13);
+            this.Lbl_Nome.TabIndex = 0;
+            this.Lbl_Nome.Text = "Nome";
+            // 
+            // Lbl_Sobrenome
+            // 
+            this.Lbl_Sobrenome.AutoSize = true;
+            this.Lbl_Sobrenome.Location = new System.Drawing.Point(111, 159);
+            this.Lbl_Sobrenome.Name = "Lbl_Sobrenome";
+            this.Lbl_Sobrenome.Size = new System.Drawing.Size(61, 13);
+            this.Lbl_Sobrenome.TabIndex = 1;
+            this.Lbl_Sobrenome.Text = "Sobrenome";
+            // 
+            // Lbl_Idade
+            // 
+            this.Lbl_Idade.AutoSize = true;
+            this.Lbl_Idade.Location = new System.Drawing.Point(111, 186);
+            this.Lbl_Idade.Name = "Lbl_Idade";
+            this.Lbl_Idade.Size = new System.Drawing.Size(34, 13);
+            this.Lbl_Idade.TabIndex = 2;
+            this.Lbl_Idade.Text = "Idade";
+            // 
+            // Lbl_Bairro
+            // 
+            this.Lbl_Bairro.AutoSize = true;
+            this.Lbl_Bairro.Location = new System.Drawing.Point(111, 214);
+            this.Lbl_Bairro.Name = "Lbl_Bairro";
+            this.Lbl_Bairro.Size = new System.Drawing.Size(34, 13);
+            this.Lbl_Bairro.TabIndex = 3;
+            this.Lbl_Bairro.Text = "Bairro";
+            // 
+            // Lbl_Celular
+            // 
+            this.Lbl_Celular.AutoSize = true;
+            this.Lbl_Celular.Location = new System.Drawing.Point(111, 242);
+            this.Lbl_Celular.Name = "Lbl_Celular";
+            this.Lbl_Celular.Size = new System.Drawing.Size(39, 13);
+            this.Lbl_Celular.TabIndex = 4;
+            this.Lbl_Celular.Text = "Celular";
+            // 
+            // Lbl_Conf_dados
+            // 
+            this.Lbl_Conf_dados.AutoSize = true;
+            this.Lbl_Conf_dados.Location = new System.Drawing.Point(111, 270);
+            this.Lbl_Conf_dados.Name = "Lbl_Conf_dados";
+            this.Lbl_Conf_dados.Size = new System.Drawing.Size(115, 13);
+            this.Lbl_Conf_dados.TabIndex = 5;
+            this.Lbl_Conf_dados.Text = "Confirmação de Dados";
+            // 
+            // Txt_Nome
+            // 
+            this.Txt_Nome.Location = new System.Drawing.Point(243, 127);
+            this.Txt_Nome.Name = "Txt_Nome";
+            this.Txt_Nome.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Nome.TabIndex = 6;
+            // 
+            // Txt_Sobrenome
+            // 
+            this.Txt_Sobrenome.Location = new System.Drawing.Point(243, 152);
+            this.Txt_Sobrenome.Name = "Txt_Sobrenome";
+            this.Txt_Sobrenome.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Sobrenome.TabIndex = 7;
+            // 
+            // Txt_Idade
+            // 
+            this.Txt_Idade.Location = new System.Drawing.Point(243, 179);
+            this.Txt_Idade.Name = "Txt_Idade";
+            this.Txt_Idade.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Idade.TabIndex = 8;
+            // 
+            // Txt_Bairro
+            // 
+            this.Txt_Bairro.Location = new System.Drawing.Point(243, 211);
+            this.Txt_Bairro.Name = "Txt_Bairro";
+            this.Txt_Bairro.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Bairro.TabIndex = 9;
+            // 
+            // Txt_Cellular
+            // 
+            this.Txt_Cellular.Location = new System.Drawing.Point(243, 235);
+            this.Txt_Cellular.Name = "Txt_Cellular";
+            this.Txt_Cellular.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Cellular.TabIndex = 10;
+            // 
+            // Txt_Conf_Dados
+            // 
+            this.Txt_Conf_Dados.Location = new System.Drawing.Point(243, 263);
+            this.Txt_Conf_Dados.Name = "Txt_Conf_Dados";
+            this.Txt_Conf_Dados.Size = new System.Drawing.Size(279, 20);
+            this.Txt_Conf_Dados.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(584, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(584, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(584, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(584, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(584, 260);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 52);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Dados Conpletos";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Frm_cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cadastro_2.Properties.Resources.asian_business_woman_manager_analyzing_datum_charts_typing_computer_making_notes_documents_table_office_vintage_color_selective_focus_business_concept;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Txt_Conf_Dados);
+            this.Controls.Add(this.Txt_Cellular);
+            this.Controls.Add(this.Txt_Bairro);
+            this.Controls.Add(this.Txt_Idade);
+            this.Controls.Add(this.Txt_Sobrenome);
+            this.Controls.Add(this.Txt_Nome);
+            this.Controls.Add(this.Lbl_Conf_dados);
+            this.Controls.Add(this.Lbl_Celular);
+            this.Controls.Add(this.Lbl_Bairro);
+            this.Controls.Add(this.Lbl_Idade);
+            this.Controls.Add(this.Lbl_Sobrenome);
+            this.Controls.Add(this.Lbl_Nome);
+            this.DoubleBuffered = true;
+            this.Name = "Frm_cadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Nome;
+        private System.Windows.Forms.Label Lbl_Sobrenome;
+        private System.Windows.Forms.Label Lbl_Idade;
+        private System.Windows.Forms.Label Lbl_Bairro;
+        private System.Windows.Forms.Label Lbl_Celular;
+        private System.Windows.Forms.Label Lbl_Conf_dados;
+        private System.Windows.Forms.TextBox Txt_Nome;
+        private System.Windows.Forms.TextBox Txt_Sobrenome;
+        private System.Windows.Forms.TextBox Txt_Idade;
+        private System.Windows.Forms.TextBox Txt_Bairro;
+        private System.Windows.Forms.TextBox Txt_Cellular;
+        private System.Windows.Forms.TextBox Txt_Conf_Dados;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastro_2
+{
+    public partial class Frm_cadastro : Form
+    {
+        public Frm_cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
